967de93e310a9e61fdc925df21cdff7bcca0b83c
Brent Roman <brent@mbari.org>
2026-04-22 14:36:34 -0700

    bumped date

--- version.h
+++ version.h
@@ -3,17 +3,17 @@
 //(for improved efficiency and compatibility with Ruby 1.9 and newer)
 
 #define RUBY_VERSION "1.8.9"
-#define RUBY_RELEASE_DATE "2021-11-29"
+#define RUBY_RELEASE_DATE "2026-04-21"
 #define RUBY_VERSION_CODE 189
-#define RUBY_RELEASE_CODE 20211129
+#define RUBY_RELEASE_CODE 20260421
 #define RUBY_PATCHLEVEL 352
 
 #define RUBY_VERSION_MAJOR 1
 #define RUBY_VERSION_MINOR 8
 #define RUBY_VERSION_TEENY 9
 #define RUBY_RELEASE_YEAR 2021
-#define RUBY_RELEASE_MONTH 11
-#define RUBY_RELEASE_DAY 29
+#define RUBY_RELEASE_MONTH 4
+#define RUBY_RELEASE_DAY 21
 
 #ifdef RUBY_EXTERN
 RUBY_EXTERN const char ruby_version[];
